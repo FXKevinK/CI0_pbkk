5c3e75b3c68a925b1efc0057f665ee99cac86b56
FXKevinK <kevinkoes.fx@gmail.com>
2021-06-08 20:52:46 +0700

    Initial commit

---
+++ application/views/profile.php
@@ -0,0 +1,148 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  
+    <title>Quiz1-FXKK (Profile)</title>
+    <meta content="" name="descriptison">
+    <meta content="" name="keywords">
+   
+    <!-- Favicons -->
+    <!-- <link href="../assets/img/logo.png" rel="icon"> -->
+    <!-- <link href="../assets/img/logo.png" rel="logo"> -->
+    
+    
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    
+    <!-- Vendor CSS Files -->
+    <!-- <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"> -->
+    <link href="<?php echo base_url('assets/vendor/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
+    <!-- <link href="../assets/vendor/icofont/icofont.min.css" rel="stylesheet"> -->
+    <link href="<?php echo base_url('assets/vendor/icofont/icofont.min.css')?>" rel="stylesheet">
+    <!-- <link href="../assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet"> -->
+    <link href="<?php echo base_url('assets/vendor/owl.carousel/assets/owl.carousel.min.css')?>" rel="stylesheet">
+    <!-- <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet"> -->
+    <link href="<?php echo base_url('assets/vendor/boxicons/css/boxicons.min.css')?>" rel="stylesheet">
+    <!-- <link href="../assets/vendor/venobox/venobox.css" rel="stylesheet"> -->
+    <link href="<?php echo base_url('assets/vendor/venobox/venobox.css')?>" rel="stylesheet">
+    <!-- <link href="../assets/vendor/aos/aos.css" rel="stylesheet"> -->
+    <link href="<?php echo base_url('assets/vendor/aos/aos.css')?>" rel="stylesheet">
+    
+    <!-- Template Main CSS File -->
+    <!-- <link href="../css/style.css" rel="stylesheet"> -->
+    <link href="<?php echo base_url('css/style.css')?>" rel="stylesheet">
+    
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container-fluid d-flex justify-content-between align-items-center">
+
+    <h1 class="logo">FXKevinK</h1>
+    <!-- Uncomment below if you prefer to use an image logo -->
+    <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+    <nav class="nav-menu d-none d-lg-block">
+        <ul>
+            <li class="active">Profile</li>
+        </ul>
+    </nav><!-- .nav-menu -->
+
+    </div>
+
+    </header><!-- End Header -->
+
+    <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about propic">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Who am I?</h2>
+          <p>Hello there! Let me introduce myself ~~~</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4">
+            <!-- <img src="../assets/img/about.jpg" class="img-fluid card-2 imeg" alt="Kevin Picture"> -->
+            <img src="<?php echo base_url('assets/img/about.jpg')?>" class="img-fluid card-2 imeg" alt="Kevin Picture">
+          </div>
+          <div class="col-lg-8 pt-4 pt-lg-0 content">
+            <h3>Fransiskus Xaverius Kevin Koesnadi</h3>
+            <p class="font-italic">
+              05111840000162 - Informatics - FTEIC ITS
+            </p>
+            <div class="row">
+              <div class="col-lg-6">
+                <ul>
+                    <li><strong>-> Web:</strong><a href="https://fxkevink180162.blogspot.com"> https://fxkevink180162.blogspot.com</a></li>
+                    <li><strong>-> City:</strong> Bogor, WJ, Indonesia</li>
+                    <li><strong>-> Phone:</strong> (+62)87770278529</li>
+                    <li><strong>-> Email:</strong> kevinkoes.fx@gmail.com</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section><!-- End About Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>FXKevinK</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/kelly-free-bootstrap-cv-resume-html-template/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- End  Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top"><i class="bx bx-up-arrow-alt"></i></a>
+
+  <!-- Vendor JS Files -->
+  <!-- <script src="../assets/vendor/jquery/jquery.min.js"></script> -->
+  <script src="<?php echo base_url('assets/vendor/jquery/jquery.min.js')?>"></script>
+  <!-- <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script> -->
+  <script src="<?php echo base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
+  <!-- <script src="../assets/vendor/jquery.easing/jquery.easing.min.js"></script> -->
+  <script src="<?php echo base_url('assets/vendor/jquery.easing/jquery.easing.min.js')?>"></script>
+  <!-- <script src="../assets/vendor/php-email-form/validate.js"></script> -->
+  <script src="<?php echo base_url('assets/vendor/php-email-form/validate.js')?>"></script>
+  <!-- <script src="../assets/vendor/waypoints/jquery.waypoints.min.js"></script> -->
+  <script src="<?php echo base_url('assets/vendor/waypoints/jquery.waypoints.min.js')?>"></script>
+  <!-- <script src="../assets/vendor/counterup/counterup.min.js"></script> -->
+  <script src="<?php echo base_url('assets/vendor/counterup/counterup.min.js')?>"></script>
+  <!-- <script src="../assets/vendor/owl.carousel/owl.carousel.min.js"></script> -->
+  <script src="<?php echo base_url('assets/vendor/owl.carousel/owl.carousel.min.js')?>"></script>
+  <!-- <script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script> -->
+  <script src="<?php echo base_url('assets/vendor/isotope-layout/isotope.pkgd.min.js')?>"></script>
+  <!-- <script src="../assets/vendor/venobox/venobox.min.js"></script> -->
+  <script src="<?php echo base_url('assets/vendor/venobox/venobox.min.js')?>"></script>
+  <!-- <script src="../assets/vendor/aos/aos.js"></script> -->
+  <script src="<?php echo base_url('assets/vendor/aos/aos.js')?>"></script>
+
+  <!-- Template Main JS File -->
+  <!-- <script src="../js/main.js"></script> -->
+  <script src="<?php echo base_url('js/main.js')?>"></script>
+
+</body>
+
+</html>
